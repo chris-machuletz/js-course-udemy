453a3fe522c1c5f19374d68e3251533cdf77b855
Christoph Machuletz <machuletz.christoph@gmail.com>
2019-11-29 18:51:53 +0100

    lecture 155 8:15min

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -5,6 +5,7 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 import Recipe from './models/Recipe';
 import List from './models/List';
@@ -90,10 +91,32 @@ const controlRecipe = async () => {
         recipeView.renderRecipe(state.recipe);
     }
 }
+
+/**
+ * LIST CONTROLLER
+ */
+const addIngredientsToShoppingList = () => {
+    // Create new List if there is none
+    if (!state.list) state.list = new List();
+    state.recipe.ingredients.forEach(el => {
+        listView.renderItem(el);
+    });
+}
+
+console.log(state);
+
+/**
+ * EVENT LISTENERS
+ */
+
 // Add two event listeners
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
-// Event listeners for increase and decrease servings buttons
+/**
+ * Event listeners:
+ * - increase and decrease servings buttons
+ * - add ingredients to shopping list
+ *  */
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.servings-dec, .servings-dec *')) {
         //decrease servings
@@ -101,9 +124,11 @@ elements.recipe.addEventListener('click', e => {
     } else if (e.target.matches('.servings-inc, .servings-inc *')) {
         //increase servings
         state.recipe.updateServings('inc');
+    } else if (e.target.matches('.ing_to_shopping-list, .ing_to_shopping-list *')) {
+        addIngredientsToShoppingList();
+
     }
     recipeView.updateServingsAndIngredients(state.recipe);
-    console.log(state.recipe);
 });
 
 window.l = new List();
